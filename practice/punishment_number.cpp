f26b17074f3eb53557b2669fd58f2df3883dd08c
Rishabh Singh <ayushsingh91200@gmail.com>
2025-02-15 18:35:04 +0530

    leetcode 2698

--- practice/punishment_number.cpp
+++ practice/punishment_number.cpp
@@ -5,7 +5,7 @@ using namespace std;
     bool check(int sq,int currSum,int num)
     {
         if(sq==0) return currSum==num;
-        return check(sq/10,currSum+sq%10,num)||check(sq/100,currSum+sq%100,num)||check(sq/1000,currSum+sq%1000,num);
+        return check(sq/10,currSum+sq%10,num)||check(sq/100,currSum+sq%100,num)||check(sq/1000,currSum+sq%1000,num)||check(sq/10000,currSum+sq%10000,num);
     }
     int punishmentNumber(int n) {
         int punish=0;
