0213ece66a69914855821143599718d02a674cbc
Rishabh Singh <106950554+rishabh-xyd@users.noreply.github.com>
2025-03-21 17:08:19 +0530

    .

--- DAY 1 PATTERNS/char_tringle.c
+++
@@ -1,9 +0,0 @@
-//CHAR TRINGLE
-#include<stdio.h>
-void main(){
-    for(char i=65;i<=69;i++){
-        for(char j=65;j<=i;j++)
-        printf("%c",j);
-        printf("\n");
-    }
-}
--- DAY 2/13_gcd_lcm.c
+++
@@ -1,1 +0,0 @@
-//gcd and lcm
